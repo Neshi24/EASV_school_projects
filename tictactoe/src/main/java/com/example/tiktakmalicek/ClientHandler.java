5284e71d778afd4bc7274a657d981ed80dc0ac38
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2021-10-11 14:58:26 +0200

    removed tictactoe server & tictactoe server
    we decided to start from a provided template and build on top of it
    
    the current state is a functioning tictactoe game 3x3 with no special additions to it

--- tictactoe/src/main/java/com/example/tiktakmalicek/ClientHandler.java
+++
@@ -1,8 +0,0 @@
-package com.example.tiktakmalicek;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-
-
--- tictactoe/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tiktakmalicek {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.tiktakmalicek to javafx.fxml;
-    exports com.example.tiktakmalicek;
-}
