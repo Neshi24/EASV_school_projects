7c106446c6178dc67b5bbf9f705f3faaf283655f
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2021-10-12 14:01:56 +0200

    working for now

---
+++ tictactoe/gui/controller/TicTacViewController.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe.gui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import tictactoe.bll.*;
+
+
+/**
+ *
+ * @author Stegger
+ */
+public class TicTacViewController implements Initializable
+{
+
+    @FXML
+    private Label lblPlayer;
+
+    @FXML
+    private Button btnNewGame;
+
+    @FXML
+    private GridPane gridPane;
+    
+    private static final String TXT_PLAYER = "Player: ";
+    private GameBoard game;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event)
+    {
+        try
+        {
+            if(game.canDoNextTurn()){
+                Integer row = GridPane.getRowIndex((Node) event.getSource());
+                Integer col = GridPane.getColumnIndex((Node) event.getSource());
+                int r = (row == null) ? 0 : row;
+                int c = (col == null) ? 0 : col;
+                int player = game.turnToPlayerNumber();
+                if (game.play(c, r))
+                {
+                    Button btn = (Button) event.getSource();
+                    String xOrO = player == 1 ? "O" : "X";
+                    btn.setText(xOrO);
+                    if (game.isGameOver())
+                    {
+                        int winner = game.getWinner();
+                        displayWinner(winner);
+                    } else {
+                        player = game.getNextPlayer();
+                        setPlayer();
+                    }
+                }
+            }
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @FXML
+    private void handleNewGame(ActionEvent event)
+    {
+        game.newGame();
+        setPlayer();
+        clearBoard();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        game = new GameBoard();
+        setPlayer();
+    }
+
+    private void setPlayer()
+    {
+        lblPlayer.setText(TXT_PLAYER + game.turnToPlayerNumber() + " is on turn");
+    }
+
+    private void displayWinner(int winner)
+    {
+        String message = "";
+        switch (winner)
+        {
+            case -1:
+                message = "It's a draw :-(";
+                break;
+            default:
+                message = "Player " + winner + " wins!!!";
+                break;
+        }
+        lblPlayer.setText(message);
+    }
+
+    private void clearBoard()
+    {
+        for(Node n : gridPane.getChildren())
+        {
+            Button btn = (Button) n;
+            btn.setText("");
+        }
+    }
+
+}
